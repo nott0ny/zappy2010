050311c523b0ebbb2beb49ac22b5d36bdc3e9cb4
rouxalban <rouxalban@45e6fad5-e6d6-7ab7-b36b-7769f023fefc>
2010-05-31 13:30:09 +0000

    multi client //a clean

--- server/sources/wait_clients.c
+++ server/sources/wait_clients.c
@@ -5,7 +5,7 @@
 ** Login   <mouafi_a@epitech.net>
 ** 
 ** Started on  Fri May  7 10:58:37 2010 amine mouafik
-** Last update Thu May 13 16:54:11 2010 amine mouafik
+** Last update Mon May 31 15:21:24 2010 alban roux
 */
 
 #include <string.h>
@@ -19,60 +19,99 @@
 #include <stdio.h>
 #include "server.h"
 
-void		display_received(int cs)
-{
-  char		buf[BUFSIZ];
-  int		offset;
+/**/
+#define MAX(a,b)	( (a>b)?(a):(b) )
+void	alloc_fd(t_network *e,int fd);
 
-  offset = read(cs, buf, sizeof(buf));
-  if (offset == 0)
-    exit(0);
-  write(1, buf, offset);
-  return ;
+void	std_read(__attribute__((unused))t_env *e, int cur)
+{
+  printf("in str_read %d\n", cur);
 }
 
-void		new_client(t_env *e)
+void	do_fd(t_env *e)
 {
-  t_clients	*client;
-  static int	id;
+  int	cur;
 
-  client = xmalloc(sizeof(t_clients));
-  client->cs = e->network->cs;
-  client->id = ++id;
-  client->players = NULL;
-  client->next = e->clients;
-  e->clients = client;
-  printf("New client pushed : cs(%d) id(%d)\n", client->cs, client->id);
-}
-
-int		manage_clients(t_env *e)
-{
-  fd_set	readf;
-
-  printf("Writing 'BIENVENUE\\n' on %d ...\n", e->clients->cs);
-  write(e->clients->cs, "BIENVENUE\n", strlen("BIENVENUE\n"));
-  while (1)
+  cur = 0;
+  while (cur < e->network->max_fd_used + 1)
     {
-      FD_ZERO(&readf);
-      FD_SET(e->clients->cs, &readf);
-      if (select(e->clients->cs + 1, &readf, NULL, NULL, NULL) < 0)
-	manage_clients(e);
-      if (FD_ISSET(e->clients->cs, &readf))
-	display_received(e->clients->cs);
+      if (!e->network->fdt[cur])
+	{
+	  cur++;
+	  continue;
+	}
+      if (FD_ISSET(cur,&e->network->r))
+	e->network->fdt[cur]->in.f(e, cur);
+      if (FD_ISSET(cur,&e->network->w))
+	e->network->fdt[cur]->out.f(e, cur);
+      cur++;
     }
-  return (0);
 }
 
-int		wait_clients(t_env *e)
+static void	init_fd_set(t_network *e)
 {
-  fd_set	readf;
+  int		cur;
 
-  FD_ZERO(&readf);
-  e->clients = xmalloc(sizeof(t_clients));
-  if ((e->network->cs = accept(e->network->s,
-			       (struct sockaddr *)&e->network->client,
-			       &e->network->csl)) > 0)
-    new_client(e);
-  manage_clients(e);
-  return (0);
+  cur = 0;
+  FD_ZERO(&e->r);
+  FD_ZERO(&e->w);
+  while (cur < e->max_fd_used + 1)
+    {
+      if (!e->fdt[cur])
+	{
+	  cur++;
+	  continue;
+	}
+      if (e->fdt[cur]->type & T_READ)
+	  FD_SET(cur,&e->r);
+      if (e->fdt[cur]->type & T_WRITE)
+	  FD_SET(cur,&e->w);
+      cur++;
+    }
+}
+
+void	set_max_fd_used(t_network *e, int fd)
+{
+  e->max_fd_used = MAX(e->max_fd_used, fd);
+}
+
+void			new_connection(t_env *e,int fd_conn)
+{
+  struct sockaddr_in	name;
+  socklen_t		namelen;
+  int			fd;
+  char			addr[4];
+
+  namelen = sizeof(name);
+  fd = accept(fd_conn,(struct sockaddr *)&name, &namelen);
+  memcpy(&addr,&name.sin_addr.s_addr,sizeof(name.sin_addr.s_addr));
+  if (fd < 0)
+    {
+      perror("ARRG accept");
+      exit(1);
+    }
+  alloc_fd(e->network, fd);
+  e->network->fdt[fd]->type |= T_READ;
+  e->network->fdt[fd]->in.f = std_read;
+  set_max_fd_used(e->network, fd);
+  write(fd, "BIENVENUE\n", strlen("BIENVENUE\n"));
+}
+
+int	wait_clients(t_env *e)
+{
+  int	nfds;
+
+  while (42)
+    {
+      init_fd_set(e->network);
+      nfds = select(e->network->max_fd_used + 1, &e->network->r, &e->network->w, 0, 0);
+      if (nfds)
+	do_fd(e);
+      else
+	{
+	  perror("ARRG select");
+	  exit(1);
+	}
+    }
+  return 0;
 }

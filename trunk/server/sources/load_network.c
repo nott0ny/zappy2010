472606729ddfe86f3ab1246dbfcc384c39762d6f
imkyotoweb <imkyotoweb@45e6fad5-e6d6-7ab7-b36b-7769f023fefc>
2010-06-11 00:39:44 +0000

    Timeout setted at 0.

--- trunk/server/sources/load_network.c
+++ trunk/server/sources/load_network.c
@@ -29,6 +29,8 @@ static t_network	*init_network(t_params *params)
   network = Xmalloc(sizeof(*network));
   X(NULL, memset(network, 0, sizeof(*network)), "memset");
   network->port = params->port;
+  network->timeout.tv_sec = 0;
+  network->timeout.tv_usec = 0;
   return (network);
 }
 

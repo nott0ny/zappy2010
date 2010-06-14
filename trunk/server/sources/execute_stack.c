c6e1fb972200fab55a65cb24ce1d0d59590619ab
imkyotoweb <imkyotoweb@45e6fad5-e6d6-7ab7-b36b-7769f023fefc>
2010-06-14 13:03:09 +0000

    Ajout execute_stack() dans wait_clients et execute_stack.c : Work in Progress ! Timeout select dans init_timeout : mettre a jour en fonction de la stack.

---
+++ trunk/server/sources/execute_stack.c
@@ -0,0 +1,16 @@
+/*
+** execute_stack.c for zappy in /u/all/mouafi_a/cu/rendu/c/zappy2010/server
+** 
+** Made by amine mouafik
+** Login   <mouafi_a@epitech.net>
+** 
+** Started on  Mon Jun 14 14:52:14 2010 amine mouafik
+** Last update Mon Jun 14 14:53:58 2010 amine mouafik
+*/
+
+#include "server.h"
+
+void	execute_stack(t_env *e)
+{
+  e = e;
+}

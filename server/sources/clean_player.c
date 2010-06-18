e1667ad61b02a450e4055d63153e740de027a1b8
imkyotoweb <imkyotoweb@45e6fad5-e6d6-7ab7-b36b-7769f023fefc>
2010-06-18 10:49:32 +0000

    Done with this fuckin' shit ! Keep on correcting warnings.

---
+++ server/sources/clean_player.c
@@ -0,0 +1,33 @@
+/*
+** clean_player.c for zappy in /u/all/mouafi_a/cu/rendu/c/zappy2010/server
+**
+** Made by amine mouafik
+** Login   <mouafi_a@epitech.net>
+**
+** Started on  Mon Jun 14 14:52:14 2010 amine mouafik
+** Last update Mon Jun 14 15:34:09 2010 amine mouafik
+*/
+
+#include <sys/socket.h>
+#include <stdlib.h>
+
+#include "server.h"
+#include "clean_player.h"
+
+void		clean_player(t_env *e, t_players *player)
+{
+  t_players	*flush;
+
+  shutdown(player->fd_associate, SHUT_RDWR);
+  close_fd(e->network, player->fd_associate);
+  player->stacklast = NULL;
+  remove_player_stack(e->execution, player);
+  flush = player;
+  player = player->next;
+  free(flush->wr_rb->buffer);
+  free(flush->rd_rb->buffer);
+  free(flush->wr_rb);
+  free(flush->rd_rb);
+  free(flush->bag);
+  free(flush);
+}

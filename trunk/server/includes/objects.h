51bd20238ca8ca300985c07e8772aab620e8fe68
imkyotoweb <imkyotoweb@45e6fad5-e6d6-7ab7-b36b-7769f023fefc>
2010-06-19 10:49:26 +0000

    Objects goes with gl_objects

---
+++ trunk/server/includes/objects.h
@@ -0,0 +1,12 @@
+#ifndef __OBJECTS_H__
+# define __OBJECTS_H__
+
+# define NOURRITURE	0
+# define LINEMATE	1
+# define DERAUMERE	2
+# define SIBUR		3
+# define MENDIANE	4
+# define PHIRAS		5
+# define THYSTAME	6
+
+#endif

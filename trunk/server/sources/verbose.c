b528d70e5ee6f1ac1aebc7fbd47220c5c6d54a7d
imkyotoweb <imkyotoweb@45e6fad5-e6d6-7ab7-b36b-7769f023fefc>
2010-06-19 14:24:31 +0000

    verbose.c added

---
+++ trunk/server/sources/verbose.c
@@ -0,0 +1,32 @@
+
+/*
+** stdread.c for zappy in /u/all/mouafi_a/cu/rendu/c/zappy2010/server/sources
+**
+** Made by amine mouafik
+** Login   <mouafi_a@epitech.net>
+**
+** Started on  Mon Jun  7 15:05:51 2010 amine mouafik
+** Last update Tue Jun 15 12:07:34 2010 amine mouafik
+*/
+
+#include <sys/time.h>
+#include <stdio.h>
+
+#include "types.h"
+#include "utils.h"
+
+void	verbose(int output, int len, char *verbose, t_players *player)
+{
+  struct timeval	timestamp;
+
+  gettimeofday(&timestamp, NULL);
+  verbose[len - 1] = '\0';
+  if (output)
+    printf("[%d:%.6d][->] Sent to #%d : %s%s%s\n",
+	   (int)timestamp.tv_sec, (int)timestamp.tv_usec,
+	   player->fd_associate, PURPLE, verbose, WHITE);
+  else
+    printf("[%d:%.6d][<-] Received from #%d : %s%s%s\n",
+	   (int)timestamp.tv_sec, (int)timestamp.tv_usec,
+	   player->fd_associate, CYAN, verbose, WHITE);
+}
